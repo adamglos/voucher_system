eb4827bdbee4dfc7e99deb73b12f58a4b73109a4
adamg <adamglos@gmail.com>
2025-06-01 15:46:51 +0200

    csrf_trusted_origins

--- voucher_system/settings.py
+++ voucher_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1eukh2pqn(bjl0_a5$ia1gq9c1jtxxw4$se()0qyue5^-bv&3g'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! :)
 DEBUG = True
 
 ALLOWED_HOSTS = [
